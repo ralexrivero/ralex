f3c4156881d08d381833952c66ad54b2068659b7
ralexrivero <ralexrivero@gmail.com>
2023-04-01 15:15:25 -0300

    sidebar with colapsable content

--- portfolio/src/components/MainContent.jsx
+++ portfolio/src/components/MainContent.jsx
@@ -3,8 +3,17 @@ import React from 'react';
 const MainContent = () => {
   return (
     <main>
-      <h1>Titulo principal</h1>
-      <p>Parrafo</p>
+      <div className="container">
+        <h1>Ronald Alexander</h1>
+        <span>FullStack & Machine Learning Developer </span>
+        <h2>¡Bienvenidos a mi sitio web personal!</h2>
+
+        <p>Soy un fullstack developer y machine learning specialist, apasionado por la tecnología y comprometido con la creación de soluciones innovadoras y eficientes que transformen el mundo. Mi propósito es utilizar mi experiencia y habilidades técnicas para mejorar la vida de las personas y optimizar los procesos de las empresas.</p>
+        <p>Como líder en la implementación de nuevas tecnologías, mi objetivo es ser un modelo a seguir para otros desarrolladores y contribuir al crecimiento de la comunidad de desarrolladores. Me apasiona el desafío de enfrentar problemas complejos y encontrar soluciones creativas y efectivas, utilizando las últimas tecnologías y herramientas disponibles.</p>
+        <p>Además, creo que es importante trabajar en equipo y colaborar con otros profesionales para lograr objetivos comunes. Por lo tanto, siempre estoy buscando oportunidades para aprender de mis colegas y compartir mis propias ideas y perspectivas para lograr soluciones innovadoras y efectivas.</p>
+        <p>Mis valores son la honestidad, la integridad y la excelencia. Creo que es importante ser honesto en todo lo que hago, tanto en mi trabajo como en mi vida personal. También valoro la integridad, es decir, ser coherente entre lo que digo y lo que hago, y actuar con ética en todo momento. Por último, la excelencia es un valor fundamental para mí. Siempre trato de hacer lo mejor posible en todo lo que hago y busco la perfección en mi trabajo.</p>
+        <p>Si buscas soluciones tecnológicas innovadoras y efectivas, has llegado al lugar correcto. Estoy aquí para ayudarte a encontrar la mejor solución para tus necesidades. ¡Contáctame y trabajemos juntos para transformar el mundo a través de la tecnología!</p>
+      </div>
     </main>
   )
 }
--- portfolio/src/components/Sidebar.jsx
+++ portfolio/src/components/Sidebar.jsx
@@ -1,12 +1,65 @@
-import React from "react";
-
+import { useState } from "react";
 
 const Sidebar = () => {
+  const [expandedIndexes, setExpandedIndexes] = useState([]);
+
+  const handleButtonClick = (index) => {
+    setExpandedIndexes((prevIndexes) =>
+      prevIndexes.includes(index)
+        ? prevIndexes.filter((i) => i !== index)
+        : [...prevIndexes, index]
+    );
+  };
+
+  const getExpandedIndex = () => {
+    const expandedIndex = [];
+    document.querySelectorAll(".btn-collapse").forEach((button, index) => {
+      button.addEventListener("click", () => {
+        if (expandedIndex.includes(index)) {
+          expandedIndex.splice(expandedIndex.indexOf(index), 1);
+        } else {
+          expandedIndex.push(index);
+        }
+        setExpandedIndexes(expandedIndex);
+      });
+    });
+  };
+
   return (
     <aside>
-      <h1>Sidebar</h1>
+      <div className="container">
+        <h2 className="fs-md p-t-md p-b-md m-b-lg border-bottom">En esta página</h2>
+        <nav>
+          <ul className="m-l-md list-reset">
+            <li>
+              <button className="btn btn-collapse m-t-md" onClick={() => handleButtonClick(0)}>Tema 1</button>
+              <ul className={`content-list m-l-md ${expandedIndexes.includes(0) ? "" : "hide"}`}>
+                <li className="fs-xs">sub tema 1</li>
+                <li className="fs-xs">sub tema 2</li>
+                <li className="fs-xs">sub tema 3</li>
+              </ul>
+            </li>
+            <li>
+              <button className="btn btn-collapse m-t-md" onClick={() => handleButtonClick(1)}>Tema 2</button>
+              <ul className={`content-list m-l-md ${expandedIndexes.includes(1) ? "" : "hide"}`}>
+                <li className="fs-xs">sub tema 4</li>
+                <li className="fs-xs">sub tema 5</li>
+                <li className="fs-xs">sub tema 6</li>
+              </ul>
+            </li>
+            <li>
+              <button className="btn btn-collapse m-t-md" onClick={() => handleButtonClick(2)}>Tema 3</button>
+              <ul className={`content-list m-l-md ${expandedIndexes.includes(2) ? "" : "hide"}`}>
+                <li className="fs-xs">sub tema 7</li>
+                <li className="fs-xs">sub tema 8</li>
+                <li className="fs-xs">sub tema 9</li>
+              </ul>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </aside>
-  )
-}
+  );
+};
 
 export default Sidebar;
