8cf836989f52c68cb863fdf91b4def5dd92e0644
ralexrivero <ralexrivero@gmail.com>
2023-05-28 18:59:12 -0300

    main design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
       <nav className="nav-header" aria-label="Menu de navegación principal">
         <ul id="nav-header" className="nav-header-container" >
           <li><a className="nav-header-links active active-first" href="#" aria-current="page"><span className="material-symbols-rounded">home</span></a></li>
-          <li><a className="nav-header-links" href="#"><span className="material-symbols-rounded">person</span>mi otro yo</a></li>
+          <li><a className="nav-header-links" href="#"><span className="material-symbols-rounded">person</span>soy</a></li>
           <li><a className="nav-header-links" href="#"><span className="material-symbols-rounded">check_circle</span>auto análisis</a></li>
           <li><a className="nav-header-links" href="#"><span className="material-symbols-rounded">work</span>para que veas</a></li>
           <li><a className="nav-header-links" href="#"><span className="material-symbols-rounded">feed</span>según yo</a></li>
