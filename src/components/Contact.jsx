ad62a0a7c4f885fa9c8a4518a1c5854010df5019
ralexrivero <ralexrivero@gmail.com>
2023-06-13 15:05:24 -0300

    add form with formspree

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,57 @@
+import { useForm, ValidationError } from '@formspree/react';
+
 const Contact = () => {
+  const [state, handleSubmit] = useForm("xlekkear");
+  if (state.succeeded) {
+      return <p>Thanks for joining!</p>;
+  }
+
   return (
-    <h1>Contact page</h1>
+    <section>
+      <form onSubmit={handleSubmit}>
+        <input
+          id="name"
+          type="text"
+          name="name"
+          placeholder="Name"
+          required
+        />
+        <ValidationError
+          prefix="Name"
+          field="name"
+          errors={state.errors}
+        />
+        <input
+          id="email"
+          type="email"
+          name="email"
+          placeholder="Email"
+          required
+        />
+        <ValidationError
+          prefix="Email"
+          field="email"
+          errors={state.errors}
+        />
+        <textarea
+          id="message"
+          name="message"
+          placeholder="Message"
+          required
+        />
+        <ValidationError
+          prefix="Message"
+          field="message"
+          errors={state.errors}
+        />
+        <button
+          type="submit"
+          disabled={state.submitting}
+        >
+          Submit
+        </button>
+      </form>
+    </section>
   )
 }
 
