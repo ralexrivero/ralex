621db0a060cdac7f07155b8b99f59c978eb933e5
ralexrivero <ralexrivero@gmail.com>
2023-06-14 00:14:45 -0300

    style and custom messagge for form send

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,17 @@
 import { useForm, ValidationError } from '@formspree/react';
+import { Link } from 'react-router-dom';
 
 const Contact = () => {
   const [state, handleSubmit] = useForm("xlekkear");
   if (state.succeeded) {
-      return <p>Thanks for joining!</p>;
+      return (
+        <div className="success-message">
+        <p>¡Genial! 🎉 ¡Tu mensaje fue enviado! 💌</p>
+        <p>¡Mil gracias por tomarte el tiempo de escribirme! 🙌 Valoro tus palabras y te prometo que te voy a contestar a la brevedad. Mientras tanto, dale una mirada a la página y conóceme un poco más. 🌟</p>
+        <p>¡Espero que el sitio te guste y te parezca buena onda! 💫✨</p>
+        <Link to="/" className="success-message-link"><span className="material-symbols-rounded">home</span></Link>
+      </div>
+      );
   }
 
   return (
--- src/components/Self.jsx
+++ src/components/Self.jsx
@@ -1,7 +1,14 @@
+import { Link } from 'react-router-dom';
+
 const Self = () => {
   return (
-    <h1>Self</h1>
-  )
+    <div className="success-message">
+      <p>¡Genial! 🎉 ¡Tu mensaje fue enviado! 💌</p>
+      <p>¡Mil gracias por tomarte el tiempo de escribirme! 🙌 Valoro tus palabras y te prometo que te voy a contestar a la brevedad. Mientras tanto, dale una mirada a la página y conóceme un poco más. 🌟</p>
+      <p>¡Espero que el sitio te guste y te parezca buena onda! 💫✨</p>
+      <Link to="/" className="success-message-link"><span className="material-symbols-rounded">home</span></Link>
+    </div>
+  );
 }
 
 export default Self;
