6ce71366f6421300819be0181be866df713c5571
ralexrivero <ralexrivero@gmail.com>
2023-06-19 02:23:39 -0300

    add postgresql and react.js course overview

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
-import { SiHtml5, SiCss3, SiJavascript } from '@icons-pack/react-simple-icons';
+import { SiHtml5, SiCss3, SiJavascript, SiPostgresql, SiReact } from '@icons-pack/react-simple-icons';
 import Tag from "./search/Tag";
 import Level from './Level';
 
 const Courses = () => {
   return (
     <section className="section-courses">
+      {/** Frontend101 */}
       <article className="course-overview">
         <h2 className="h2-highlight">Frontend 101</h2>
         <p>Fundamentos del desarrollo Web desde cero</p>
@@ -19,7 +20,6 @@ const Courses = () => {
           </div>
           <div className="course-level">
             <Level level="1" />
-
           </div>
         <div className="course-description">
           <p>Frontend 101 es el curso perfecto para adentrarte en el emocionante mundo del desarrollo web frontend. Aprenderás desde los conceptos fundamentales hasta las tecnologías más relevantes, como HTML, CSS y JavaScript, construyendo una base sólida para crear páginas web impactantes.</p>
@@ -34,6 +34,54 @@ const Courses = () => {
             <Tag filter="Curso" />
           </div>
       </article>
+      {/** PostgresSql101 */}
+      <article className="course-overview">
+        <h2 className="h2-highlight">Introducción a PostgreSQL</h2>
+        <p>Aprende los fundamentos de PostgreSQL</p>
+        <div className="card-technologies">
+          <h2>Tecnologías</h2>
+          <ul className="project-links">
+            <li className="badge-tech"><SiPostgresql title="PostgreSQL" color="default" /></li>
+          </ul>
+        </div>
+    <div className="course-level">
+      <Level level="1" />
+    </div>
+    <div className="course-description">
+      <p>El curso de Introducción a PostgreSQL te ofrece la oportunidad de sumergirte en el fascinante mundo de las bases de datos relacionales. Aprenderás desde los conceptos básicos hasta las técnicas más avanzadas de PostgreSQL, adquiriendo los conocimientos necesarios para administrar y utilizar eficazmente esta poderosa base de datos.</p>
+      <p>Explora ejemplos prácticos, laboratorios y material teórico diseñado para guiarte en el proceso de comprensión de PostgreSQL. Con el curso de Introducción a PostgreSQL, estarás preparado para trabajar con bases de datos, escribir consultas avanzadas y desarrollar aplicaciones con esta tecnología líder en la industria.</p>
+    </div>
+    {/* <Link className="link-btn" to="/courses/intro-postgresql/">Ver más</Link> */}
+    <div className="tags">
+      <Tag filter="Base de datos" />
+      <Tag filter="PostgreSQL" />
+      <Tag filter="Curso" />
+    </div>
+      </article>
+      {/** React.js */}
+      <article className="course-overview">
+  <h2 className="h2-highlight">Introducción a React.js</h2>
+  <p>Aprende los fundamentos de React.js</p>
+  <div className="card-technologies">
+    <h2>Tecnologías</h2>
+    <ul className="project-links">
+      <li className="badge-tech"><SiReact title="React.js" color="default" /></li>
+    </ul>
+  </div>
+  <div className="course-level">
+    <Level level="1" />
+  </div>
+  <div className="course-description">
+    <p>El curso de Introducción a React.js te ofrece una emocionante oportunidad de sumergirte en el apasionante mundo del desarrollo web frontend con React.js. Desde los conceptos básicos hasta las técnicas más avanzadas, adquirirás los conocimientos necesarios para crear aplicaciones web interactivas y dinámicas.</p>
+    <p>Sumérgete en una experiencia de aprendizaje enriquecedora a través de proyectos prácticos, desafiantes laboratorios y material teórico cuidadosamente diseñado para guiarte en el proceso de comprensión de React.js. Con el curso de Introducción a React.js, estarás preparado para crear interfaces de usuario modernas y atractivas, gestionar eficientemente el estado de tus aplicaciones y aprovechar al máximo la reutilización de componentes en tus proyectos de desarrollo frontend.</p>
+  </div>
+  {/* <Link className="link-btn" to="/courses/intro-react/">Ver más</Link> */}
+  <div className="tags">
+    <Tag filter="Frontend" />
+    <Tag filter="React.js" />
+    <Tag filter="Curso" />
+  </div>
+</article>
     </section>
   )
 }
--- src/components/courses/Frontend101.jsx
+++ src/components/courses/Frontend101.jsx
@@ -25,7 +25,43 @@ const Frontend101 = () => {
           <p>El enfoque de este curso se centra en brindarte una sólida base de conocimientos y habilidades. Aprenderás desde los fundamentos hasta los aspectos más avanzados de cada uno de estos lenguajes de programación. Al dominar estas herramientas esenciales, estarás preparado para crear páginas web de manera efectiva y profesional.</p>
           <p>El curso se estructura de forma práctica y orientada a la experiencia. Podrás participar en emocionantes proyectos, realizar laboratorios interactivos y explorar ejemplos de código prácticos. Además, tendrás acceso a material teórico exhaustivo que te guiará en cada paso del proceso de construcción de páginas web estáticas.</p>
           <p>Frontend 101 es el punto de partida ideal para aquellos que deseen adentrarse en el apasionante mundo de la programación y el desarrollo frontend. No se requiere experiencia previa, ya que el curso está diseñado para llevarte desde cero hasta la capacidad de crear tus propias aplicaciones web.</p>
-
+        </div>
+        <div className="course-content">
+          <h2>Listado del Curso</h2>
+          <h3>Módulo de HTML:</h3>
+          <ol>
+            <li>Introducción a Internet y la web</li>
+            <li>Estructura básica de un documento HTML</li>
+            <li>Etiquetas y elementos HTML fundamentales</li>
+            <li>Creación de enlaces y navegación entre páginas</li>
+            <li>Uso de imágenes y multimedia en HTML</li>
+            <li>Tablas y formularios en HTML</li>
+            <li>Semántica HTML para mejorar la accesibilidad y SEO</li>
+            <li>Práctica y ejemplos de código HTML</li>
+          </ol>
+          <h3>Módulo de CSS:</h3>
+          <ol start="9">
+            <li>Introducción a CSS y su importancia en el diseño web</li>
+            <li>Selectores y propiedades CSS básicas</li>
+            <li>Manipulación de texto y fuentes en CSS</li>
+            <li>Diseño de cajas y posicionamiento de elementos con CSS</li>
+            <li>Estilización de enlaces y navegación</li>
+            <li>Creación de diseños responsivos con CSS media queries</li>
+            <li>Animaciones y transformaciones CSS</li>
+            <li>Práctica y ejemplos de código CSS</li>
+          </ol>
+          <h3>Módulo de JavaScript:</h3>
+          <ol start="17">
+            <li>Introducción a JavaScript y su papel en la interactividad web</li>
+            <li>Variables, tipos de datos y operadores en JavaScript</li>
+            <li>Control de flujo y estructuras de control en JavaScript</li>
+            <li>Funciones y eventos en JavaScript</li>
+            <li>Manipulación del DOM con JavaScript</li>
+            <li>Trabajo con arrays y objetos en JavaScript</li>
+            <li>Introducción a jQuery y su uso en la interacción con el DOM</li>
+            <li>Proyecto final: Aplicación web interactiva utilizando HTML, CSS y JavaScript</li>
+          </ol>
+          <p className="project-resume">Cada módulo del curso se abordará de manera exhaustiva, brindando ejemplos prácticos y actividades para reforzar los conceptos aprendidos. Al finalizar el curso, tendrás los conocimientos necesarios para desarrollar tus propias páginas web con HTML, CSS y JavaScript, así como para crear aplicaciones web interactivas utilizando estas tecnologías.</p>
         </div>
         <Link className="link-btn" to="/courses/">Cursos</Link>
         <div className="tags">
