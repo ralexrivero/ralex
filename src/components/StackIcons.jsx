1d7b6671fd5c1abd64931aae22c36e006cba8631
ralexrivero <ralexrivero@gmail.com>
2023-06-20 21:28:02 -0300

    fix icons display

--- src/components/StackIcons.jsx
+++ src/components/StackIcons.jsx
@@ -4,19 +4,31 @@ const StackIcons = (props) => {
   const { icons } = props;
   const iconsArray = Array.isArray(icons) ? icons : icons.split(',');
 
+  const iconComponents = {
+    SiHtml5,
+    SiCss3,
+    SiJavascript,
+    SiPostgresql,
+    SiReact,
+    SiPython,
+    SiDjango,
+    SiMongodb,
+    SiMysql,
+    SiC
+  };
+
   return (
-      <ul className="project-links">
-          {iconsArray.map((icon, index) => {
-            const IconComponent = icon;
-            return (
-              <li key={index} className="badge-tech">
-                <IconComponent  title={icon} color="default" />
-              </li>
-            )
-          })}
-      </ul>
+    <ul className="project-links">
+      {iconsArray.map((icon, index) => {
+        const IconComponent = iconComponents[icon];
+        return (
+          <li key={index} className="badge-tech">
+            {IconComponent && <IconComponent title={icon} color="default" />}
+          </li>
+        );
+      })}
+    </ul>
   );
 };
-
 
 export default StackIcons;
